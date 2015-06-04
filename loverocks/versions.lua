0936ed5780fc04e81037b891cd10dd82cebf8523
Kyle McLamb <kjmclamb@gmail.com>
2015-06-04 10:48:41 -0400

    Add lua 5.1 to rocks_provided
    This overrides the lua version used to run luarocks, which love doesn't
    use anyways.

--- loverocks/versions.lua
+++ loverocks/versions.lua
@@ -1,27 +1,32 @@
 local log = require 'loverocks.log'
 
 -- FIXME: non-love version numbers are pretty much made up
+-- FIXME: Add the luajit rocks-provided modules too
 local STABLE = "0.9.2"
 local versions = {
 	["0.9.0"] = {
+		lua        = "5.1-1",
 		love       = "0.9.0-1",
 		luasocket  = "2.0.2-5",
 		enet       = "1.2-1",
 		utf8       = "0.1.0-1"
 	},
 	["0.9.1"] = {
+		lua        = "5.1-1",
 		love       = "0.9.1-1",
 		luasocket  = "2.0.2-5",
 		enet       = "1.2-1",
 		utf8       = "0.1.0-1"
 	},
 	["0.9.2"] = {
+		lua        = "5.1-1",
 		love       = "0.9.2-1",
 		luasocket  = "2.0.2-5",
 		enet       = "1.2-1",
 		utf8       = "0.1.0-1"
 	},
 	["0.10.0"] = {
+		lua        = "5.1-1",
 		love       = "0.10.0-1",
 		luasocket  = "2.0.2-5",
 		enet       = "1.2-1",

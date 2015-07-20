3445d8e6564015884359625a222b80b7e059fb6d
Kyle McLamb <kjmclamb@gmail.com>
2015-07-20 16:48:52 -0400

    Export loverocks loaders, paths
    Makes the code much easier to look it <3

--- templates/love9/rocks/init.lua
+++ templates/love9/rocks/init.lua
@@ -3,54 +3,54 @@
 --  (c) Kyle McLamb, 2015 <alloyed@tfwno.gf>, MIT License.
 --  TODO: Configurable prefixes
 
-local function inject()
-	local luarocks_paths = {
-		"rocks/share/lua/5.1/?.lua",
-		"rocks/share/lua/5.1/?/init.lua",
-	}
-	local luarocks_cpaths = {
-		"rocks/lib/lua/5.1/?"
-	}
-	if love.filesystem.getRequirePath then -- 0.10
-		error('Update to match 0.9')
-		local paths = {love.filesystem.getRequirePath(), unpack(luarocks_paths)}
-		local path = table.concat(paths, ';')
-		love.filesystem.setRequirePath(path)
+local luarocks_paths = {
+	"rocks/share/lua/5.1/?.lua",
+	"rocks/share/lua/5.1/?/init.lua",
+}
+local luarocks_cpaths = {
+	"rocks/lib/lua/5.1/?"
+}
 
-		package.path = ""
-	else -- 0.9, maybe lower
-		local function loader(modname)
-			local modpath = modname:gsub('%.', '/')
-			for _, elem in ipairs(luarocks_paths) do
-				elem = elem:gsub('%?', modpath)
-				if love.filesystem.exists(elem) then
-					return love.filesystem.load(elem)
-				end
-			end
-			return "\n\tno module '" .. modname .. "' in LOVERocks path."
+local function loader(modname)
+	local modpath = modname:gsub('%.', '/')
+	for _, elem in ipairs(luarocks_paths) do
+		elem = elem:gsub('%?', modpath)
+		if love.filesystem.exists(elem) then
+			return love.filesystem.load(elem)
 		end
+	end
+	return "\n\tno module '" .. modname .. "' in LOVERocks path."
+end
 
-		local function c_loader(mod_name)
-			local ext = love.system.getOS() == 'windows' and ".dll" or ".so"
-			local file = mod_name:gsub("%.", "/") .. ext
-			local fn   = mod_name:gsub("%.", "_")
+local function c_loader(mod_name)
+	local ext = love.system.getOS() == 'windows' and ".dll" or ".so"
+	local file = mod_name:gsub("%.", "/") .. ext
+	local fn   = mod_name:gsub("%.", "_")
 
-			for _, elem in ipairs(luarocks_cpaths) do
-				elem = elem:gsub('%?', file)
-				local real_dir = love.filesystem.getRealDirectory(elem)
-				if real_dir then
-					return package.loadlib(real_dir .. "/" .. elem, "luaopen_" .. fn)
-				end
-				return"\n\tno library '" .. file .. "' in LOVERocks path."
-			end
-
+	for _, elem in ipairs(luarocks_cpaths) do
+		elem = elem:gsub('%?', file)
+		local real_dir = love.filesystem.getRealDirectory(elem)
+		if real_dir then
+			local path = real_dir .. "/" .. elem
+			return package.loadlib(path, "loveopen_" .. fn) or
+			       package.loadlib(path, "luaopen_" .. fn)
 		end
-		table.insert(package.loaders, loader)
-		table.insert(package.loaders, c_loader)
-
-		package.path = ""
-		package.cpath = ""
+		return"\n\tno library '" .. file .. "' in LOVERocks path."
 	end
 end
 
-return setmetatable({inject = inject}, {__call = inject})
+local function inject()
+	table.insert(package.loaders, loader)
+	table.insert(package.loaders, c_loader)
+
+	package.path = ""
+	package.cpath = ""
+end
+
+return setmetatable({
+	inject = inject,
+	path = luarocks_paths,
+	cpath = luarocks_cpaths,
+	loader = loader,
+	c_loader = c_loader
+}, {__call = inject})

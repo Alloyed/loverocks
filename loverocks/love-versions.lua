0c8f22bc3e6ab2bc7c760c50b9ba8802c102b584
Kyle McLamb <kjmclamb@gmail.com>
2016-05-07 08:39:43 -0400

    Update stable love version 0.10.0 -> 0.10.1

--- loverocks/love-versions.lua
+++ loverocks/love-versions.lua
@@ -4,7 +4,7 @@ local T = require 'loverocks.schema'
 
 -- FIXME: non-love version numbers are pretty much made up
 -- FIXME: Add the luajit rocks-provided modules too
-local STABLE = "0.10.0"
+local STABLE = "0.10.1"
 local versions = {
 	["0.9.0"] = {
 		lua        = "5.1-1",
@@ -30,6 +30,13 @@ local versions = {
 	["0.10.0"] = {
 		lua        = "5.1-1",
 		love       = "0.10.0-1",
+		luasocket  = "2.0.2-5",
+		enet       = "1.2-1",
+		utf8       = "0.1.0-1"
+	},
+	["0.10.1"] = {
+		lua        = "5.1-1",
+		love       = "0.10.1-1",
 		luasocket  = "2.0.2-5",
 		enet       = "1.2-1",
 		utf8       = "0.1.0-1"
